2a15ab3964bd3dee98d875855c451f613cce8c1c
GerminalCorreia <GerminalCorreia@users.noreply.github.com>
2019-12-15 04:20:23 +0100

    Added setHearder in app.js

--- API/app.js
+++ API/app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-Width, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
